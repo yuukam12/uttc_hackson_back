1071308fd60d3d296c1b8176fa27a46d65b9b972
yuukam12 <chococookie147@gmail.com>
2023-11-12 15:00:39 +0900

    Sun Nov 12 15:00:39 JST 2023

--- main.go
+++ main.go
@@ -124,7 +124,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
 		w.Write(bytes)
 		return
 
